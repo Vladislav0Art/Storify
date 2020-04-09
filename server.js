08e9f9d905e60351971af4555794e6f45935843b
TommyIQ <bmp110676@gmail.com>
2020-04-09 19:15:42 +0300

    :(

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use('/api/slides', slides);
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
   // Set a static folder
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
